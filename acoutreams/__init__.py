32f0462bfdd24d65341ebee5d31ef798a7466a2c
Nikita Ustimenko <nikita@tfplx104.tfp.kit.edu>
2024-12-19 18:55:03 +0100

    Update README.md

--- acoutreams/__init__.py
+++ acoutreams/__init__.py
@@ -45,7 +45,7 @@ Functions
 
 """
 
-_version__ = "0.1.16"
+_version__ = "0.1.17"
 
 from scipy.special import (  # noqa: F401
     hankel1,
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
 
     name='acoutreams',
 
-    version='0.1.16',
+    version='0.1.17',
 
     author='Nikita Ustimenko',
 
