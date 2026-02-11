Corrects calculation of Mie coefficients for a sphere

acoustotreams/__init__.py
@@ -149,7 +149,7 @@ Functions imported from treams.special, treams.misc, and treams.lattice
 
 """
 
-_version__ = "0.2.5"
+_version__ = "0.2.6"
 
 from scipy.special import (  # noqa: F401
     hankel1,
bdf95b9e6e80edc24d9ac910d348b6d7f09c3035
Nikita Ustimenko <nikita@tfplx119.tfp.kit.edu>
2026-02-11 15:48:49 +0100

    Corrects calculation of Mie coefficients for a sphere

--- acoustotreams/__init__.py
+++ acoustotreams/__init__.py
@@ -149,7 +149,7 @@ Functions imported from treams.special, treams.misc, and treams.lattice
 
 """
 
-_version__ = "0.2.5"
+_version__ = "0.2.6"
 
 from scipy.special import (  # noqa: F401
     hankel1,
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
 
     name='acoustotreams',
 
-    version='0.2.5',
+    version='0.2.6',
 
     author='Nikita Ustimenko',
 
